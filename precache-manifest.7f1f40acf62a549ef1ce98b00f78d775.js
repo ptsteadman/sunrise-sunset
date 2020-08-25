fb6047d1abdff7bd00288002a9d2ca8956585c66
ptsteadman <ptsteadman@gmail.com>
2020-08-25 19:47:25 +0000

    Deploying to gh-pages from  @ e1d2ec13f3a59e30fda98f00c55997528d5d6978 🚀

---
+++ precache-manifest.7f1f40acf62a549ef1ce98b00f78d775.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32fd062724540545f292fa20a779c941",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd090936319d991d8a4d",
+    "url": "/static/css/main.b49d71bd.chunk.css"
+  },
+  {
+    "revision": "2d721b85f6ab139bab4d",
+    "url": "/static/js/2.b8db504d.chunk.js"
+  },
+  {
+    "revision": "bd090936319d991d8a4d",
+    "url": "/static/js/main.a503ead5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
