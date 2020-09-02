d52aeef6327494d5a3c6c229f59a011ee1887c93
Patrick Steadman <ptsteadman@gmail.com>
2020-09-02 18:56:37 -0400

    add sun
    Signed-off-by: Patrick Steadman <ptsteadman@gmail.com>

--- src/components/Lights/index.js
+++ src/components/Lights/index.js
@@ -4,9 +4,15 @@ export default () => {
   return (
     <group>
       <ambientLight intensity={0.1} />
+      <mesh position={[-200, -10, 0]}>
+        <sphereBufferGeometry attach="geometry" args={[4, 32, 32]} />
+        <meshBasicMaterial attach="material" color="#FFFF99" fog={false} />
+        <pointLight distance={6100} intensity={0.3} color={0xffffcc} />
+      </mesh>
       <directionalLight
         intensity={0.5}
         position={[-1, 0, 0]}
+        color='white'
       />
     </group>
   );
