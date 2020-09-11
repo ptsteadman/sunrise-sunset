4e478628461fc48d51c081fef6a0b33c5097ad45
ptsteadman <ptsteadman@gmail.com>
2020-09-11 20:43:09 +0000

    Deploying to gh-pages from  @ 4901bc16e3cdb7179e696fbf76a163dd262dc7a3 🚀

---
+++ precache-manifest.a23bffcd41ae7ae1d61daaabb2b3243a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f055a98287093495d24659b8531d64e",
+    "url": "/sunrise-sunset/index.html"
+  },
+  {
+    "revision": "93a90bed4557b074e7dd",
+    "url": "/sunrise-sunset/static/css/main.775d42a6.chunk.css"
+  },
+  {
+    "revision": "a36948fa3f52c6d73e3f",
+    "url": "/sunrise-sunset/static/js/2.0daaeb89.chunk.js"
+  },
+  {
+    "revision": "359dabd766c22964ee9a922c36528893",
+    "url": "/sunrise-sunset/static/js/2.0daaeb89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a90bed4557b074e7dd",
+    "url": "/sunrise-sunset/static/js/main.27a60232.chunk.js"
+  },
+  {
+    "revision": "59e912a7fcf5232881bf",
+    "url": "/sunrise-sunset/static/js/runtime-main.641aec74.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sunrise-sunset/precache-manifest.a1de8e13c2f5ee35c2f3068c0e108e3d.js"
+  "/sunrise-sunset/precache-manifest.a23bffcd41ae7ae1d61daaabb2b3243a.js"
 );
 
 self.addEventListener('message', (event) => {
