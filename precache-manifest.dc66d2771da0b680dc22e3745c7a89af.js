62619bc8ff1a0e7b7a900a0372213d02b18bcd00
ptsteadman <ptsteadman@gmail.com>
2020-09-30 15:24:54 +0000

    Deploying to gh-pages from  @ 15e01cff502fd5c2dfffe2c6fba7f398565c108f 🚀

---
+++ precache-manifest.dc66d2771da0b680dc22e3745c7a89af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24363c8738e3dfc02de90f320df51176",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6348d7e0613b13796419",
+    "url": "/static/css/main.e8b53f69.chunk.css"
+  },
+  {
+    "revision": "ae1f519be182b0f965be",
+    "url": "/static/js/2.4e436dd9.chunk.js"
+  },
+  {
+    "revision": "359dabd766c22964ee9a922c36528893",
+    "url": "/static/js/2.4e436dd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6348d7e0613b13796419",
+    "url": "/static/js/main.7e1580bf.chunk.js"
+  },
+  {
+    "revision": "6426d2f4a2e4a825baf4",
+    "url": "/static/js/runtime-main.6991344e.js"
+  },
+  {
+    "revision": "23045eccaaabc1c0b58fd5f0d4a5e75d",
+    "url": "/static/media/AkzidenzGroteskBE-XBdCnIt.23045ecc.woff2"
+  },
+  {
+    "revision": "cee9b5bd0035ca8497234fa762bf9fa4",
+    "url": "/static/media/AkzidenzGroteskBE-XBdCnIt.cee9b5bd.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sunrise-sunset/precache-manifest.6526020e474debcd630be9632f35eed6.js"
+  "/precache-manifest.dc66d2771da0b680dc22e3745c7a89af.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sunrise-sunset/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
