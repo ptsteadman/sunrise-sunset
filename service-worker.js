41d1af4a1f49b414f3e7b7b0e1819cf079a05bd8
ptsteadman <ptsteadman@gmail.com>
2020-10-05 08:12:37 +0000

    Deploying to gh-pages from  @ adb7f8297a88df63f0437ae8856ed9ad9d65fd71 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sunrise-sunset/precache-manifest.e2f6eb576a78f6ed4e5e3652757c02c4.js"
+  "/sunrise-sunset/precache-manifest.ad835c92ab562b4a8291b411ffd42ae3.js"
 );
 
 self.addEventListener('message', (event) => {
